6109ff7e8b27eb953c5bd6223bc75685525aa5e1
Rodgger Bruno <rodggerbruno@gmail.com>
2017-10-04 12:19:41 -0400

    platform compatibility on log file

---
+++ lib/w_log.c
@@ -0,0 +1,27 @@
+
+#include <telebot.h>
+
+static const char *directory_log;
+
+char *get_file(long int user_log, enum Etype type) {
+
+}
+
+bool insert_file(long int user_log, enum Etype type, char *current) {
+
+}
+
+bool cp_file(long int user_log, enum Etype type, char *current) {
+
+}
+
+bool text_log(long int user_log, char *extra, long int date, char *text) {
+
+}
+
+void log_init(){
+	directory_log = get_log();
+
+
+
+}
