b101cb738cf00799b6a9bcf349a367e597843610
Rodgger Bruno <rodggerbruno@gmail.com>
2017-10-13 01:55:55 -0400

    commit initial log

--- lib/l_log.c
+++ lib/l_log.c
@@ -1,26 +1,140 @@
 #include <telebot.h>
 
 static const char *directory_log;
+static bool exist = false;
 
-char *get_file(long int user_log, enum Etype type) {
+char *get_file(long int user_log, enum Etype type, char *file_name) {
+	if(exist == true){
+		char *path;
+		size_t path_len;
 
+		path_len = strlen(directory_log) + 40 + strlen(file_name);
+		path = malloc(path_len);
+
+		if(type == t_image)
+			snprintf(path, path_len, "%s/%lu/image/%s", directory_log, user_log, file_name);
+		else if(type == t_document)
+			snprintf(path, path_len, "%s/%lu/document/%s", directory_log, user_log, file_name);
+		else if(type == t_voice)
+			snprintf(path, path_len, "%s/%lu/voice/%s", directory_log, user_log, file_name);
+		else if(type == t_audio)
+			snprintf(path, path_len, "%s/%lu/audio/%s", directory_log, user_log, file_name);
+		else if(type == t_video)
+			snprintf(path, path_len, "%s/%lu/video/%s", directory_log, user_log, file_name);
+		else if(type == t_contact)
+			snprintf(path, path_len, "%s/%lu/contact/%s", directory_log, user_log, file_name);
+
+
+		if(access_log(path) == 1)
+			return path;
+	}
+
+	return NULL;
 }
 
-bool insert_file(long int user_log, enum Etype type, char *current) {
+int access_log(const char *path){
+	if(access(path, F_OK) == -1) {
+		if(errno == ENOENT){
+			return 0; /* does not exist */
+		}
 
+		return -1; /* error */
+	}
+
+	return 1; /* exist */
 }
 
-bool cp_file(long int user_log, enum Etype type, char *current) {
+char *insert_file(long int user, enum Etype type, char *name_file) {
+	char *path_user;
+	char *full_path;
+	size_t path_user_len, full_path_len;
+	int returned;
 
+	if(exist == true){
+		path_user_len = strlen(directory_log) + 25;
+		path_user = malloc(path_user_len);
+		snprintf(path_user, path_user_len , "%s/%lu", directory_log, user);
+
+		if((returned == access_log(path_user)) == -1)
+			goto log_error;
+
+		if(returned == 0){
+			if(mkdir(path_user, S_IRWXU|S_IRWXU|S_IXGRP|S_IROTH|S_IXOTH) == -1)
+				goto log_error;
+		}
+
+
+		full_path_len = strlen(path_user) + 25 + strlen(name_file);
+		full_path = malloc(full_path_len);
+
+		if(type == t_image)
+			snprintf(full_path, full_path_len, "%s/image/", path_user);
+		else if(type == t_document)
+			snprintf(full_path, full_path_len, "%s/document/", path_user);
+		else if(type == t_voice)
+			snprintf(full_path, full_path_len, "%s/voice/", path_user);
+		else if(type == t_audio)
+			snprintf(full_path, full_path_len, "%s/audio/", path_user);
+		else if(type == t_video)
+			snprintf(full_path, full_path_len, "%s/video/", path_user);
+		else if(type == t_contact)
+			snprintf(full_path, full_path_len, "%s/contact/", path_user);
+
+
+		if((returned == access_log(full_path)) == -1)
+			goto log_error_user;
+
+		if(returned == 0){
+			if(mkdir(full_path, S_IRWXU|S_IRWXU|S_IXGRP|S_IROTH|S_IXOTH) == -1)
+				goto log_error_user;
+		}
+
+		strncat(full_path, name_file, strlen(name_file));
+
+		free(path_user);
+
+		return full_path;
+
+	}
+
+	return NULL;
+
+log_error:
+	free(path_user);
+	return name_file;
+
+log_error_user:
+	free(path_user);
+	free(full_path);
+	return name_file;
 }
 
 bool text_log(long int user_log, char *extra, long int date, char *text) {
+	if(exist == true){
+		char *path;
+		size_t path_len;
 
+		path_len = strlen(directory_log) + 25;
+		path = malloc(path_len);
+
+		snprintf(path, path_len, "%s/%lu");
+
+		if(access_log(path) == -1){
+
+		}
+	}
 }
 
 void log_init(){
 	directory_log = get_log();
 
-
-
+	if(mkdir(directory_log, S_IRWXU | S_IRWXU | S_IXGRP | S_IROTH | S_IXOTH) == -1){
+		if(errno == EEXIST){
+			exist = true;
+		}
+		else {
+			printf("error in the path to the log . . . verify config\npress enter to continue without log");
+			getchar();
+		}
+	}
 }
