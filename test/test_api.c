46f12396846cfa4b7a185a5ca3a8ef8029facf60
Ródgger Bruno <rodggerbruno@gmail.com>
2017-11-14 14:09:38 -0300

    test all methods

--- test/test_api.c
+++ test/test_api.c
@@ -3,12 +3,14 @@
 typedef struct _function {
 	char * function;
 	char * parameter;
+	char * _return;
 	int count_parameter;
 } Functions;
 
 Functions * function;
 
 void set_function();
+void verify_function();
 
 int main(int argc, char * argv[]){
 	set_function();
@@ -19,19 +21,25 @@ int main(int argc, char * argv[]){
 
 void set_function(){
 	Functions f[] = {
-		{"telebot", "token:str", 1},
+		{"telebot", "token:str", "bot:obj", 1},
 
-		{"get_updates", "bot:obj extra:str", 2},
+		{"get_updates", "bot:obj extra:str", "update:obj", 2},
 
-		{"send_message_channel", "bot:obj chat_id:str \
-text:str parse_mode:str disable_web_page_preview:bool \
-disable_notification:bool reply_to_message_id:long reply_markup:str", 8},
+		{"send_message_channel", "bot:obj chat_id:long text:str parse_mode:str\
+disable_web_page_preview:bool disable_notification:bool reply_to_message_id:long\
+reply_markup:str", "message:obj", 8},
 
-		{"send_message_chat", "bot:obj chat_id:long \
-text:str parse_mode:str disable_web_page_preview:bool \
-disable_notification:bool reply_to_message_id:long reply_markup:str", 8},
-
+		{"set_chat_title_chat", "bot:obj chat_id:long title:str", ":int", 3},
+		{"get_chat_chat", "bot:obj, chat_id:str", "chat:obj", 2},
+		{"get_chat_member_chat", "bot:obj chat_id:long user_id:long", "", 3}
 	};
+}
 
+void verify_function(){
+	Functions * ffunction;
+	int i;
 
+	for(i = 0; function; i++){
+		
+	}
 }
