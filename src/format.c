d766466f44e331f7ea42a17e958a81cd301596ce
Ródgger Bruno <rodggerbruno@gmail.com>
2018-05-18 16:01:44 -0400

    replace strlen by fstrlen(macro) rename IF_STRING_NULL to STRING_NULL deleted struct reply_markup

--- src/format.c
+++ src/format.c
@@ -6,40 +6,6 @@
 #define STRINT 13 
 
 
-char *escape_string(char *str) {
-	CURL *curl = curl_easy_init();
-	if (curl) {
-		char *out = curl_easy_escape(curl, str, 0);
-		return out;
-	}
-	return NULL;
-}
-
-char *remove_newline(char *str){
-	size_t size_str;
-
-	size_str = strlen(str);
-
-	if(str[size_str - 1] == '\n')
-		str[size_str - 1] = '\0';
-
-	return str;
-}
-
-char *format (char *formats, ...) {
-
-	va_list params;
-	va_start(params, formats);
-	
-	char *buffer = (char *)calloc(1, MAX_URL_SZ);
-	
-	vsprintf(buffer, formats, params);
-
-	va_end(params);
-
-	return buffer;
-}
-
 char *vsformat (char *formats, va_list params) {
 	char *buffer = (char *) calloc(1, MAX_URL_SZ);
 
